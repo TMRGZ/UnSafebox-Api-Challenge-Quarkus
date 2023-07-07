a0871b7ebd5ed2f9f2778de68d3bb6d7b51ff56a
TMRGZ <miguelrg5605@outlook.es>
2023-07-08 01:10:07 +0200

    FEATURE: Added exceptions

---
+++ src/main/java/com/rviewer/skeletons/domain/exception/SafeboxDoesNotExistException.java
@@ -0,0 +1,4 @@
+package com.rviewer.skeletons.domain.exception;
+
+public class SafeboxDoesNotExistException extends UnsafeboxException {
+}
---
+++ src/main/java/com/rviewer/skeletons/domain/exception/UnsafeboxException.java
@@ -0,0 +1,7 @@
+package com.rviewer.skeletons.domain.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class UnsafeboxException extends RuntimeException {
+}
