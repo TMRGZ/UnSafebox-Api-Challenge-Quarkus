7c2ddaccd79c440e57bd1ce6642f2ba73049dc24
TMRGZ <miguelrg5605@outlook.es>
2023-07-07 22:40:48 +0200

    FEATURE: Replaced Java-JUnit to Groovy-Spock

---
+++ src/test/groovy/com/rviewer/skeletons/infrastructure/controllers/PingControllerSpec.groovy
@@ -0,0 +1,34 @@
+package com.rviewer.skeletons.infrastructure.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.hamcrest.Matchers.containsString
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PingControllerSpec extends Specification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    def "getPing_success"() throws Exception {
+        expect:
+        mockMvc
+                .perform(get("/ping"))
+                .andExpect(status().isOk())
+    }
+
+    def "getPing_returnsPong"() throws Exception {
+        expect:
+        mockMvc
+                .perform(get("/ping"))
+                .andExpect(content().string(containsString("pong")))
+    }
+}
---
+++ src/test/groovy/com/rviewer/skeletons/infrastructure/persistence/PostgresConnectorSpec.groovy
@@ -0,0 +1,32 @@
+package com.rviewer.skeletons.infrastructure.persistence
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import spock.lang.Specification
+import spock.lang.Subject
+
+@SpringBootTest
+class PostgresConnectorSpec extends Specification {
+
+    @Subject
+    @Autowired
+    private PostgresConnector postgresConnector
+
+    private JdbcTemplate jdbcTemplate
+
+    void setup() {
+        jdbcTemplate = Mock(JdbcTemplate)
+    }
+
+    def "getConnectionStatus_shouldReturn2"() {
+        given:
+        jdbcTemplate.queryForObject("SELECT 1+1", Integer) >> 2
+
+        when:
+        int status = postgresConnector.getConnectionStatus()
+
+        then:
+        status == 2
+    }
+}
