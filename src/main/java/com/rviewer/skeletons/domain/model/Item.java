ef2ca0a2e900d9bc4f4ba28b10951a40d1baba41
TMRGZ <miguelrg5605@outlook.es>
2023-07-08 16:36:01 +0200

    FEATURE: Added item domain model

--- src/main/java/com/rviewer/skeletons/domain/model/Item.java
+++ src/main/java/com/rviewer/skeletons/domain/model/Item.java
@@ -1,4 +1,18 @@
 package com.rviewer.skeletons.domain.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder(toBuilder = true)
 public class Item {
+
+    private String content;
+
+    private Long safeboxId;
+
 }
