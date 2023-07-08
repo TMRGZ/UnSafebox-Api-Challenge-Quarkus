1ddf887567b93e15814df058086019caa3918a74
TMRGZ <miguelrg5605@outlook.es>
2023-07-08 22:09:18 +0200

    FEATURE: Implemented basic auth

--- src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.rviewer.skeletons.infrastructure.config;
 
+import com.rviewer.skeletons.domain.service.SafeboxService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,5 +26,11 @@ public class SecurityConfig {
                 })
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService(SafeboxService safeboxService) {
+        return username -> safeboxService.getSafebox(username)
+                .map(safebox -> new User(safebox.getName(), safebox.getPassword(), Collections.emptyList()));
     }
 }
