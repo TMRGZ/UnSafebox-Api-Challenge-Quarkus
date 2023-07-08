72710cff6ebd28bde32e9a43a6b79d6478dc77c3
TMRGZ <miguelrg5605@outlook.es>
2023-07-08 21:47:19 +0200

    FEATURE: Secured endpoints

--- src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.rviewer.skeletons.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,7 +15,11 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> {
+                    authorizeExchangeSpec.pathMatchers(HttpMethod.POST, "/safebox").permitAll();
+                    authorizeExchangeSpec.anyExchange().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
-
 }
