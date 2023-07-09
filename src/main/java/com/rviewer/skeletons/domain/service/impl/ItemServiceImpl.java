ae85f2bd43a91a1e7289769d863c358d76a07f85
TMRGZ <miguelrg5605@outlook.es>
2023-07-09 19:49:43 +0200

    FEATURE: Add loggers

--- src/main/java/com/rviewer/skeletons/domain/service/impl/ItemServiceImpl.java
+++ src/main/java/com/rviewer/skeletons/domain/service/impl/ItemServiceImpl.java
@@ -6,10 +6,12 @@ import com.rviewer.skeletons.domain.repository.ItemRepository;
 import com.rviewer.skeletons.domain.service.ItemService;
 import com.rviewer.skeletons.domain.service.SafeboxService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
@@ -19,17 +21,25 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public Flux<Item> getItems(Long safeboxId) {
-        return safeboxService.getSafebox(safeboxId).hasElement()
-                .flatMapMany(exists -> BooleanUtils.isTrue(exists)
-                        ? itemRepository.findBySafeboxId(safeboxId)
-                        : Mono.error(SafeboxDoesNotExistException::new));
+        log.info("Getting items from safebox {}", safeboxId);
+        return safeboxService.getSafebox(safeboxId).hasElement().flatMapMany(exists -> BooleanUtils.isTrue(exists)
+                ? itemRepository.findBySafeboxId(safeboxId)
+                : Mono.error(() -> {
+                    log.error("Safebox {} does not exist, cancelling...", safeboxId);
+                    return new SafeboxDoesNotExistException();
+                }
+        ));
     }
 
     @Override
     public Mono<Item> save(Long safeboxId, Item item) {
+        log.info("Storing item {} into safebox {}", item, safeboxId);
         return safeboxService.getSafebox(safeboxId)
                 .map(safebox -> item.toBuilder().safeboxId(safebox.getId()).build())
                 .flatMap(itemRepository::save)
-                .switchIfEmpty(Mono.error(SafeboxDoesNotExistException::new));
+                .switchIfEmpty(Mono.error(() -> {
+                    log.error("Safebox with id={} does not exist, cancelling...", safeboxId);
+                    return new SafeboxDoesNotExistException();
+                }));
     }
 }
--- src/main/java/com/rviewer/skeletons/domain/service/impl/SafeboxServiceImpl.java
+++ src/main/java/com/rviewer/skeletons/domain/service/impl/SafeboxServiceImpl.java
@@ -6,9 +6,11 @@ import com.rviewer.skeletons.domain.repository.SafeboxRepository;
 import com.rviewer.skeletons.domain.service.PasswordManager;
 import com.rviewer.skeletons.domain.service.SafeboxService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
 public class SafeboxServiceImpl implements SafeboxService {
 
@@ -18,13 +20,18 @@ public class SafeboxServiceImpl implements SafeboxService {
 
     @Override
     public Mono<Safebox> createSafebox(String safeboxName, String safeboxPassword) {
-        return safeboxRepository.findByNameIgnoreCase(safeboxName)
-                .hasElement().flatMap(exists -> BooleanUtils.isTrue(exists)
-                        ? Mono.error(SafeboxAlreadyExistsException::new)
-                        : safeboxRepository.save(generateEncodedSafebox(safeboxName, safeboxPassword)));
+        log.info("Creating safebox with name {} and password (hidden)", safeboxName);
+        return getSafebox(safeboxName).hasElement().flatMap(exists -> BooleanUtils.isFalse(exists)
+                ? safeboxRepository.save(generateEncodedSafebox(safeboxName, safeboxPassword))
+                : Mono.error(() -> {
+                    log.error("Safebox {} already exists, cancelling...", safeboxName);
+                    return new SafeboxAlreadyExistsException();
+                }
+        ));
     }
 
     private Safebox generateEncodedSafebox(String safeboxName, String safeboxPassword) {
+        log.info("Encoding password for new safebox {}", safeboxName);
         return Safebox.builder()
                 .name(safeboxName)
                 .password(passwordManager.encode(safeboxPassword))
@@ -33,11 +40,13 @@ public class SafeboxServiceImpl implements SafeboxService {
 
     @Override
     public Mono<Safebox> getSafebox(Long id) {
+        log.info("Retrieving safebox {}", id);
         return safeboxRepository.findById(id);
     }
 
     @Override
     public Mono<Safebox> getSafebox(String name) {
+        log.info("Retrieving safebox with name {}", name);
         return safeboxRepository.findByNameIgnoreCase(name);
     }
 }
