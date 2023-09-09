b6fd45536b30c77e984ef1cd752106d238d363f0
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-09-09 16:40:53 +0200

    FEATURE: Ported exception handler to Quarkus

--- src/main/java/com/rviewer/skeletons/infrastructure/handler/RestExceptionHandler.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/handler/RestExceptionHandler.java
@@ -2,21 +2,36 @@ package com.rviewer.skeletons.infrastructure.handler;
 
 import com.rviewer.skeletons.domain.exception.SafeboxAlreadyExistsException;
 import com.rviewer.skeletons.domain.exception.SafeboxDoesNotExistException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.rviewer.skeletons.domain.exception.UnsafeboxException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
-@RestControllerAdvice
-public class RestExceptionHandler {
-
-    @ExceptionHandler(SafeboxDoesNotExistException.class)
-    public ResponseEntity<Void> safeboxNotFound() {
-        return ResponseEntity.notFound().build();
+@Provider
+public class RestExceptionHandler implements ExceptionMapper<RuntimeException> {
+    @Override
+    public Response toResponse(RuntimeException exception) {
+        return switch (exception) {
+            case SafeboxDoesNotExistException safeboxDoesNotExistException -> toResponse(safeboxDoesNotExistException);
+            case SafeboxAlreadyExistsException safeboxAlreadyExistsException ->
+                    toResponse(safeboxAlreadyExistsException);
+            case UnsafeboxException unsafeboxException -> toResponse(unsafeboxException);
+            default -> Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        };
     }
 
-    @ExceptionHandler(SafeboxAlreadyExistsException.class)
-    public ResponseEntity<Void> safeboxAlreadyExists() {
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    private Response toResponse(SafeboxDoesNotExistException safeboxDoesNotExistException) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .build();
+    }
+
+    private Response toResponse(SafeboxAlreadyExistsException safeboxAlreadyExistsException) {
+        return Response.status(Response.Status.CONFLICT)
+                .build();
+    }
+
+    private Response toResponse(UnsafeboxException unsafeboxException) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .build();
     }
 }
