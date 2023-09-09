2e212523149160d7d8cf3ce95c36a9849ff24f39
Miguel Ángel <MiguelRG5605@Outlook.es>
2023-09-09 16:41:16 +0200

    FEATURE: Ported bean config to Quarkus

--- src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
@@ -3,22 +3,21 @@ package com.rviewer.skeletons.infrastructure.config;
 import com.rviewer.skeletons.domain.repository.ItemRepository;
 import com.rviewer.skeletons.domain.repository.SafeboxRepository;
 import com.rviewer.skeletons.domain.service.ItemService;
-import com.rviewer.skeletons.domain.service.PasswordManager;
 import com.rviewer.skeletons.domain.service.SafeboxService;
 import com.rviewer.skeletons.domain.service.impl.ItemServiceImpl;
 import com.rviewer.skeletons.domain.service.impl.SafeboxServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import jakarta.enterprise.context.Dependent;
+import jakarta.ws.rs.Produces;
 
-@Configuration
+@Dependent
 public class BeanConfig {
 
-    @Bean
-    public SafeboxService safeboxService(SafeboxRepository safeboxRepository, PasswordManager passwordManager) {
-        return new SafeboxServiceImpl(safeboxRepository, passwordManager);
+    @Produces
+    public SafeboxService safeboxService(SafeboxRepository safeboxRepository) {
+        return new SafeboxServiceImpl(safeboxRepository);
     }
 
-    @Bean
+    @Produces
     public ItemService itemService(ItemRepository itemRepository, SafeboxService safeboxService) {
         return new ItemServiceImpl(itemRepository, safeboxService);
     }
